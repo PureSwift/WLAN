cf13367d7f6ee4e026df1443fca6160fdb95f2ee
Alsey Coleman Miller <alseycmiller@gmail.com>
2018-07-05 20:35:30 -0500

    Working on WLAN

--- Sources/CSwiftLinuxWLAN/include/linux_wireless.h
+++ Sources/CSwiftLinuxWLAN/include/linux_wireless.h
@@ -70,14 +70,24 @@
 /***************************** INCLUDES *****************************/
 
 #ifdef linux
+
 #include <linux/types.h>		/* for __u* and __s* typedefs */
 #include <linux/socket.h>		/* for "struct sockaddr" et al	*/
+
 #else
 
+#include <stdint.h>
+#include <sys/types.h>
+
+typedef uint8_t __u8;
 typedef uint16_t __u16;
 typedef uint32_t __u32;
 typedef uint64_t __u64;
-typedef <#existing#> <#new#>;
+
+typedef int8_t __s8;
+typedef int16_t __s16;
+typedef int32_t __s32;
+typedef int64_t __s64;
 
 #endif
 
