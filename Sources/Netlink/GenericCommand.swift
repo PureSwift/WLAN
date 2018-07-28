23c7211178428a7d97677f20318311a899f98325
Alsey Coleman Miller <alseycmiller@gmail.com>
2018-07-28 17:56:30 -0500

    Working on Netlink

--- Sources/Netlink/GenericCommand.swift
+++ Sources/Netlink/GenericCommand.swift
@@ -41,5 +41,7 @@ extension NetlinkGenericCommand: ExpressibleByIntegerLiteral {
 
 public extension NetlinkGenericCommand {
     
+    public static let newFamily = NetlinkGenericCommand(rawValue: UInt8(CTRL_CMD_NEWFAMILY))
+    
     public static let getFamily = NetlinkGenericCommand(rawValue: UInt8(CTRL_CMD_GETFAMILY))
 }
